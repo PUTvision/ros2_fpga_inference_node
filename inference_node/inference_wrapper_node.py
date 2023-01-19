894eede77db27b9e655bbb3180f3914ecc4e2b04
Bartosz Ptak <bartptak@gmail.com>
2023-01-18 23:48:12 -0800

    Update inference node to CompressedImage

--- inference_node/inference_wrapper_node.py
+++ inference_node/inference_wrapper_node.py
@@ -17,27 +17,34 @@ class InferenceWrapperNode(Node):
         self.model_path = get_package_share_directory('inference_node') + '/data/unet_1xC32B1L2S2_640x360.xmodel'
         self.inference_runner = InferenceRunner(self.model_path)
 
-        self.segmentation_publisher = self.create_publisher(CompressedImage, '/inference', 1)
+        self.segmentation_publisher = self.create_publisher(CompressedImage, '/inference/compressed', 1)
 
         self.image_listener = self.create_subscription(CompressedImage, '/color/compressed', self.processing_callback, 1)
 
         self.get_logger().info('Waiting for image!')
 
+    @staticmethod
+    def sigmoid(x):
+        return 1 / (1 + np.exp(-x))
+
     def processing_callback(self, msg: CompressedImage):
         self.get_logger().info('I got image!')
-        img = cv2.imdecode(np.fromstring(ros_data.data, np.uint8), cv2.IMREAD_COLOR)
+        img = cv2.imdecode(np.frombuffer(msg.data, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
 
+        img = cv2.resize(img, (640,360))
         out = self.inference_runner.run_network(img).squeeze()
+
+        out = self.sigmoid(out)
+        out = np.uint8(out*255)
 
         self.publish_segmentation(out)
 
-    def publish_segmentation(mask: np.ndarray):
+    def publish_segmentation(self, mask: np.ndarray):
         msg = CompressedImage()
-        msg.header.stamp = self.get_clock().now().to_msg()
         msg.format = "jpeg"
-        msg.data = np.array(cv2.imencode('.jpg', image)[1]).tostring()
+        msg.data = np.array(cv2.imencode('.jpg', mask)[1]).tostring()
 
-        self.segmentation_publisher.pub(msg)
+        self.segmentation_publisher.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
